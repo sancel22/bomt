9bb01b396caa1c4f5e4778791ca60aff6c5df701
alvieamar <alvie.amar@bibo.com.ph>
2017-04-21 20:27:38 +0800

    tables

---
+++ database/migrations/2017_04_21_110424_create_recipients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('address');
+            $table->string('contact_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipients');
+    }
+}
