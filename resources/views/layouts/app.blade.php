035c98603499a1cb9c57fbaf3c67802050938b46
alvieamar <alvie.amar@bibo.com.ph>
2017-04-21 18:07:23 +0800

    front - login/register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BIBO GLOBAL ONLINE MONEY TRANSFER') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -36,7 +36,8 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{--{{ config('app.name', '') }}--}}
+                        BIBO GLOBAL ONLINE MONEY TRANSFER
                     </a>
                 </div>
 
