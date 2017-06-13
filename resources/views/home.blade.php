35073833d6977d5408a4d98da4e26cc613cfddfa
Cel Pitogo <cel.pitogo@bibo.com.ph>
2017-06-13 16:37:59 +0800

    Change title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Title</title>
+        <title>Bibo Online Cash Remittance</title>
         <script src="/js/app.js"></script>
         <link rel="stylesheet" type="text/css" href="/css/app.css" />
         <style type="text/css">
